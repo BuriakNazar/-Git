ed687f4bc551341f2a45abb46276a374611180ca
Назар <buriak.nazar1125@vu.cdu.edu.ua>
2025-11-08 20:21:20 +0200

    Сума числ

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine(n+m);
         }
     }
 }
